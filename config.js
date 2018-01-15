ad279eff6974d512ec7f1eb81f9d50258806e867
Tony Brancato <tonybrancato1@gmail.com>
2018-01-14 21:52:46 -0500

    added TEST_DATABASE_URL

--- config.js
+++ config.js
@@ -5,3 +5,7 @@ exports.DATABASE_URL =
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+
+exports.TEST_DATABASE_URL = (
+	process.env.TEST_DATABASE_URL ||
+	'mongodb://localhost/test-gamekeeper');
