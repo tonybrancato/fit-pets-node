e2ccda2298091b6ab7a89acd8f7d7ebd26f05a7e
Tony Brancato <tonybrancato1@gmail.com>
2018-01-18 22:42:08 -0500

    HEROKU test

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ exports.DATABASE_URL =
     global.DATABASE_URL ||
     'mongodb://localhost/fit-pets';
 exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_SECRET = process.env.JWT_SECRET || JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
 // exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || '*'; 
 exports.TEST_DATABASE_URL = (
